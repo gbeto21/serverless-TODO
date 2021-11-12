069b660b93be62885861fdad147ff42917c96586
Alberto González <albertoglezb@hotmail.com>
2021-11-11 20:12:39 -0600

    The locally conection to create a TODO was implemented.

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,30 +3,27 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-// import { getUserId } from '../utils';
-// import { createTodo } from '../../businessLogic/todos'
+import { createTodo } from "../../helpers/todos";
+import { getToken } from '../utils';
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('createTodo')
 
 export const handler =
   middy(
     async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-      const newTodo: CreateTodoRequest = JSON.parse(event.body)
-      // TODO: Implement creating a new TODO item
-      console.log("New Todo: ", newTodo);
+      const parsedBody: CreateTodoRequest = JSON.parse(event.body)
+      const jwtToken = getToken(event)
+
+      const item = await createTodo(jwtToken, parsedBody)
+
+      logger.info('item created', item)
 
       return {
-        statusCode: 200,
-        body: JSON.stringify(
-          {
-            item: {
-              todoId: "123",
-              createdAt: "2019-07-27T20:01:45.424Z",
-              name: newTodo.name,
-              dueDate: "2019-07-29T20:01:45.424Z",
-              done: false,
-              attachmentUrl: "http://example.com/image.png"
-            }
-          }
-        )
+        statusCode: 201,
+        body: JSON.stringify({
+          item
+        })
       }
     }
   )
