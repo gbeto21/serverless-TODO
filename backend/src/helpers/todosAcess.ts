d650f492259a458b4e6374eb60a1b2d1652c016a
Alberto González <albertoglezb@hotmail.com>
2021-11-13 11:27:08 -0600

    XRAY was tryied to implement with faiule.

--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,10 +1,9 @@
 import * as AWS from 'aws-sdk'
-// import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-// const XAWS = AWSXRay.captureAWS(AWS)
+const AWSXRay = require('aws-xray-sdk')
 const logger = createLogger('TodosAccess')
 
 export class TodosAccess {
@@ -216,11 +215,11 @@ export class TodosAccess {
 function createDynamoDBClient(): AWS.DynamoDB.DocumentClient {
 	if (process.env.IS_OFFLINE) {
 		logger.info('Creating a local DynamoDB instance')
-		return new AWS.DynamoDB.DocumentClient({
+		return new AWSXRay.DynamoDB.DocumentClient({
 			region: 'localhost',
 			endpoint: 'http://localhost:8000'
 		})
 	}
 
-	return new AWS.DynamoDB.DocumentClient()
+	return new AWSXRay.DynamoDB.DocumentClient()
 }
